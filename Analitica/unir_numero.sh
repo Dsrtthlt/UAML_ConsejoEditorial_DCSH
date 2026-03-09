1f6cbb89773e04a7adba5d914581cb6d4b93c7c0
Dsrt_Thlt <operezbanos@icloud.com>
2026-03-09 12:56:19 -0600

    Cambios menores
    Cambios mínimos en los archivos y su renderizado. Se actualiza script para unir archivos

--- Analitica/unir_numero.sh
+++ Analitica/unir_numero.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
 qpdf --empty --pages \
-  portada_Analitica_v_n_.pdf
-  legal_Analitica_v_n_.pdf \
-  editorial.pdf \
-  art01.pdf \
-  art02.pdf \
-  art03.pdf \
-  art04.pdf \
-  art05.pdf \
-  art06.pdf \
-  -- An@lítica_v_n_completo.pdf
+  Portada_Analitica_v1n1.pdf 1-z \
+  Legal_Analitica_v1n1.pdf 1-z \
+  Editorial_Analitica_v1n1.pdf 1-z \
+  art01_v1n1.pdf 1-z \
+  art02_v1n1.pdf 1-z \
+  art03_v1n1.pdf 1-z \
+  art04_v1n1.pdf 1-z \
+  art05_v1n1.pdf 1-z \
+  art06_v1n1.pdf 1-z \
+  -- "An@lítica_v1n1_completo.pdf"
